d2339b5a19586761b5f4738db972a8709bbb53c1
ahmetaggoz <ahmetaggoz1388@gmail.com>
2025-05-17 16:35:00 +0300

    Proje Tamamlandı.

---
+++ MyPortfolioUdemy/ViewComponents/_FeatureComponentPartial.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MyPortfolioUdemy.DAL.Context;
+
+namespace MyPortfolioUdemy.ViewComponents
+{
+    public class _FeatureComponentPartial : ViewComponent
+    {
+        MyPortfolioContext myPortfolioContext = new MyPortfolioContext();
+        public IViewComponentResult Invoke()
+        {
+            var values = myPortfolioContext.Features.ToList();
+            return View(values);
+        }
+    }
+    
+}
