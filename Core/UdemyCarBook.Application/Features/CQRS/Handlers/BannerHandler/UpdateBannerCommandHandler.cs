d5b044242057faecec587359ef5e9b4399b6e467
ahmetaggoz <ahmetaggoz1388@gmail.com>
2025-07-17 12:30:32 +0300

    Add project files.

---
+++ Core/UdemyCarBook.Application/Features/CQRS/Handlers/BannerHandler/UpdateBannerCommandHandler.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UdemyCarBook.Application.Features.CQRS.Commands.BannerCommand;
+using UdemyCarBook.Application.Interfaces;
+using UdemyCarBook.Domain.Entities;
+
+namespace UdemyCarBook.Application.Features.CQRS.Handlers.BannerHandler
+{
+    public class UpdateBannerCommandHandler
+    {
+        private readonly IRepository<Banner> _repository;
+
+        public UpdateBannerCommandHandler(IRepository<Banner> repository)
+        {
+            _repository = repository;
+        }
+        public async Task Handle(UpdateBannerCommand command)
+        {
+            var value = await _repository.GetByIdAsync(command.BannerId);
+            if (value != null)
+            {
+                value.Title = command.Title;
+                value.Description = command.Description;
+                value.VideoDescription = command.VideoDescription;
+                value.VideoUrl = command.VideoUrl;
+                await _repository.UpdateAsync(value);
+            }
+            else
+            {
+                throw new Exception("Banner not found");
+            }
+        }
+    }
+}
---
+++ Presantation/UdemyCarBook.WebApi/Controllers/BannerController.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using UdemyCarBook.Application.Features.CQRS.Commands.BannerCommand;
+using UdemyCarBook.Application.Features.CQRS.Handlers.BannerHandler;
+using UdemyCarBook.Application.Features.CQRS.Queries.BannerQueries;
+
+namespace UdemyCarBook.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BannerController : ControllerBase
+    {
+        private readonly CreateBannerCommandHandler _createBannerCommandHandler;
+        private readonly GetBannerByIdQueryHandler _getBannerByIdQueryHandler;
+        private readonly GetBannerQueryHandler _getBannerQueryHandler;
+        private readonly UpdateBannerCommandHandler _updateBannerCommandHandler;
+        private readonly RemoveBannerCommandHandler _removeBannerCommandHandler;
+
+        public BannerController(CreateBannerCommandHandler createBannerCommandHandler, GetBannerByIdQueryHandler getBannerByIdQueryHandler, GetBannerQueryHandler getBannerQueryHandler, UpdateBannerCommandHandler updateBannerCommandHandler, RemoveBannerCommandHandler removeBannerCommandHandler)
+        {
+            _createBannerCommandHandler = createBannerCommandHandler;
+            _getBannerByIdQueryHandler = getBannerByIdQueryHandler;
+            _getBannerQueryHandler = getBannerQueryHandler;
+            _updateBannerCommandHandler = updateBannerCommandHandler;
+            _removeBannerCommandHandler = removeBannerCommandHandler;
+        }
+        [HttpGet]
+        public async Task<IActionResult> BannerList()
+        {
+            var result = await _getBannerQueryHandler.Handle();
+            return Ok(result);
+        }
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetBanner(int id)
+        {
+            var result = await _getBannerByIdQueryHandler.Handle(new GetBannerByIdQueries(id));
+            return Ok(result);
+        }
+        [HttpPost]
+        public async Task<IActionResult> CreateBanner(CreateBannerCommand command)
+        {
+            await _createBannerCommandHandler.Handle(command);
+            return Ok("Banner created successfully");
+        }
+        [HttpDelete]
+        public async Task<IActionResult> RemoveBanner(RemoveBannerCommand command)
+        {
+            await _removeBannerCommandHandler.Handle(command);
+            return Ok("Banner removed successfully");
+        }
+        [HttpPut]
+        public async Task<IActionResult> UpdateBanner(UpdateBannerCommand command)
+        {
+            await _updateBannerCommandHandler.Handle(command);
+            return Ok("Banner updated successfully");
+        }
+    }
+}
